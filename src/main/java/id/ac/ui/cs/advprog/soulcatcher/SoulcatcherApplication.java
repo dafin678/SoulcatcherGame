61fbfbd4799a79ae6b9634b3e528251147f7ee12
Raissa Tito Safaraz <raissa.tito@ui.ac.id>
2022-04-20 22:35:35 +0700

    git setup

---
+++ src/main/java/id/ac/ui/cs/advprog/soulcatcher/SoulcatcherApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.soulcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulcatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulcatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/soulcatcher/SoulcatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.soulcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulcatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
