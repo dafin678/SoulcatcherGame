0bbd0e26a33c3d6b5236a71b489ae84614f25c8d
joya.ruth <joya.ruth@ui.ac.id>
2022-04-24 18:05:22 +0700

    merging

--- src/main/java/id/ac/ui/cs/advprog/soulcatcher/Security/WebSecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/soulcatcher/Security/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
