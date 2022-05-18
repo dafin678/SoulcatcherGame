62955dc09de0bf6250ad787bb4a4746f5626ba14
MUHAMMAD BINTANG AZRIEL ADITYA W <muhammad.bintang02@ui.ac.id>
2022-05-18 08:25:06 +0700

    Revert "Merge branch 'joya-upgrade' into 'master'"
    This reverts merge request !4

--- src/main/java/id/ac/ui/cs/advprog/soulcatcher/main/service/PlayerService.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.soulcatcher.main.service;
-
-import id.ac.ui.cs.advprog.soulcatcher.main.model.Player;
-
-public interface PlayerService {
-    Player createPlayer(String name);
-
-    Player getPlayer(String username);
-}
