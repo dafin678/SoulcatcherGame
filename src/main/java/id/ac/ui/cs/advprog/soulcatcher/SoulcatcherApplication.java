1bec6602a8e4128c78f1426267a6af324f3c08b5
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-06-01 23:15:38 +0700

    Microservices-setup

--- src/main/java/id/ac/ui/cs/advprog/soulcatcher/SoulcatcherApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/soulcatcher/SoulcatcherApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SoulcatcherApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SoulcatcherApplication.class, args);
     }
